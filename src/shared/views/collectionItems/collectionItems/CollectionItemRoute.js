b95d23a1fd6196ea071846a419570a28f2159428
Nick Maskell <nrmaskell@gmail.com>
2016-09-29 15:44:30 +1300

    Update CollectionItemRoute.js

--- src/shared/views/collectionItems/collectionItems/CollectionItemRoute.js
+++ src/shared/views/collectionItems/collectionItems/CollectionItemRoute.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Button from 'react-toolbox/lib/button'
 
 import { CollectionItemsListContainer } from './index'
 
 const CollectionItemsRoute = (props) => {
   return (
     <div>
-      <Button>Cool</Button>
       <CollectionItemsListContainer orderBy={props.location.query.orderBy} />
     </div>
   )
