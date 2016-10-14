78529222a3258fb5b61acd46b0d9c5a41658d54d
Ro Savage <rowan.savage@gmail.com>
2016-10-14 19:33:20 +1300

    Added ability to change museums based on url

--- src/shared/views/visit/VisitMuseumHeader/VisitMuseumHeader.js
+++ src/shared/views/visit/VisitMuseumHeader/VisitMuseumHeader.js
@@ -2,12 +2,41 @@ import React from 'react'
 
 import styles from './VisitMuseumHeader.css'
 
+const museumSettings = {
+  tepapa: {
+    name: 'TE PAPA',
+    tagline: 'Museum of New Zealand Te Papa Tongawera',
+    image: '/static/images/museums/tepapa.jpg',
+  },
+  exploratorium: {
+    name: 'EXPLORATORIUM',
+    tagline: 'the intersection of science, art, and human perception',
+    image: '/static/image/museums/exploratorium.jpg',
+  },
+}
+
+const getMuseumInfo = () => {
+  const subdomain = window.location.hostname.split('.')[0].toLowerCase()
+
+  switch (subdomain) {
+    case 'tepapa':
+      return museumSettings.tepapa
+    case 'exploratorium':
+      return museumSettings.exploratorium
+    default:
+      return museumSettings.tepapa
+  }
+}
+
+
+
 const VisitMuseumHeader = () => {
+  const museumInfo = getMuseumInfo()
   return (
-    <div className={styles.museumHeader}>
+    <div className={styles.museumHeader} style={{ backgroundImage: `url("${museumInfo.image}")` }}>
       <div className={styles.overlay}>
-        <h1 className={styles.museumName}>TE PAPA</h1>
-        <p className={styles.museumTagline}>Museum of New Zealand Te Papa Tongawera</p>
+        <h1 className={styles.museumName}>{museumInfo.name}</h1>
+        <p className={styles.museumTagline}>{museumInfo.tagline}</p>
       </div>
     </div>
   )
