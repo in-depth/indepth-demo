15c6eef2a7ce131926a3ec338778fa348e8a4118
nickmask <nrmaskell@gmail.com>
2016-10-17 12:58:09 +1300

    Ad basic event styling

--- src/shared/views/shows/shows/ShowsList/ShowsList.js
+++ src/shared/views/shows/shows/ShowsList/ShowsList.js
@@ -1,26 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-import { List, ListItem } from 'react-toolbox/lib/list'
+import styles from './ShowList.css'
 
 const ShowsList = (props) => {
   return (
     <div>
-      <h1><b>Events</b></h1>
-      <List selectable ripple>
-        {props.order.map((showId) => (
-          <div key={showId}>
-            <i style={{ padding: '0 20px' }}>{props.shows[showId].date}</i>
-            <Link to={`/events/${showId}`}>
-              <ListItem
-                avatar={props.shows[showId].imageUrl}
-                caption={props.shows[showId].title}
-                legend={`${props.shows[showId].shortDesc.substr(0, 80)}...`}
-              />
-            </Link>
-          </div>
-        ))}
-      </List>
+      {props.order.map((showId) => (
+        <div key={showId}>
+          <Link className={styles.showItem} to={`/events/${showId}`}>
+            <img className={styles.image} alt={props.shows[showId].title} src={props.shows[showId].imageUrl} />
+            <div className={styles.showText}>
+              <h3>{props.shows[showId].title}</h3>
+              <i style={{ padding: '0 20px' }}>{props.shows[showId].date}</i>
+              <i style={{ padding: '0 20px' }}>{props.shows[showId].time}</i>
+            </div>
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }
@@ -31,3 +28,12 @@ ShowsList.propTypes = {
 }
 
 export default ShowsList
+//
+//
+// <ListItem
+//   className={styles.showItem}
+//   avatar={props.shows[showId].imageUrl}
+//   caption={props.shows[showId].title}
+//   legend={`${props.shows[showId].shortDesc.substr(0, 80)}...`}
+// />
+// <p>{`${props.shows[showId].shortDesc.substr(0, 80)}...`}</p>
