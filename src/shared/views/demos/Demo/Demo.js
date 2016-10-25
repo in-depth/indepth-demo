f13265bddd11cea1920f4df9fdc4d274dd7dc68d
nickmask <nrmaskell@gmail.com>
2016-10-26 12:52:12 +1300

    Fix for SSR

--- src/shared/views/demos/Demo/Demo.js
+++ src/shared/views/demos/Demo/Demo.js
@@ -7,32 +7,33 @@ import styles from './Demo.css'
 class Demo extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { selected: 0 }
+    this.state = {
+      selected: 0,
+      demoFeatures: [
+        { value: 0, label: 'Vibrate', component: <Vibrate /> },
+        { value: 1, label: 'Fullscreen', component: <Fullscreen /> },
+        { value: 2, label: 'Voice Recognition', component: <VoiceRecognition /> },
+        { value: 3, label: 'Notifications', component: <Notifications /> },
+      ],
+    }
   }
 
   handleChange(value) {
     this.setState({ selected: value })
   }
 
-  demoFeatures = [
-    { value: 0, label: 'Vibrate', component: <Vibrate /> },
-    { value: 1, label: 'Fullscreen', component: <Fullscreen /> },
-    { value: 2, label: 'Voice Recognition', component: <VoiceRecognition /> },
-    { value: 3, label: 'Notifications', component: <Notifications /> },
-  ]
-
   render() {
-    const { selected } = this.state
+    const { selected, demoFeatures } = this.state
     return (
       <div className={styles.demoPage}>
         <span className={styles.select}>Select demo feature:</span>
         <Dropdown
           auto
           onChange={this.handleChange.bind(this)}
-          source={this.demoFeatures}
+          source={demoFeatures}
           value={selected}
         />
-        {this.demoFeatures[selected].component}
+      {demoFeatures[selected].component}
       </div>
     )
   }
