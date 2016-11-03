36c554bca14898dcb9d21213a27ab6eb04e06eb6
nickmask <nrmaskell@gmail.com>
2016-11-03 18:31:14 +1300

    Add centred desktop cards

--- src/shared/views/plans/plans/PlansList/PlansList.js
+++ src/shared/views/plans/plans/PlansList/PlansList.js
@@ -6,23 +6,25 @@ import styles from './PlansList.css'
 const PlansList = (props) => {
   return (
     <div className={styles.main}>
-      <div>
+      <div className={styles.pageHeader}>
         <h1>PLAN</h1>
       </div>
-      <p>Pick a plan below to get a custom itinerary</p>
-      <div className={styles.plansList}>
-      {props.plans.map((plan) => (
-        <div key={plan.title} className={styles.plansWrapper}>
-          <PlanLink
-            image={plan.backgroundUrl}
-            subtitle={plan.subtitle}
-            title={plan.title}
-            backgroundUrl={plan.backgroundUrl}
-            path={plan.path}
-          />
+      <p className={styles.subHeader}>Pick a plan below to get a custom itinerary</p>
+      <div className={styles.plansWrapper}>
+        <div className={styles.plansList}>
+          {props.plans.map((plan) => (
+            <div key={plan.title} className={styles.planWrapper}>
+              <PlanLink
+                image={plan.backgroundUrl}
+                subtitle={plan.subtitle}
+                title={plan.title}
+                backgroundUrl={plan.backgroundUrl}
+                path={plan.path}
+              />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
     </div>
   )
 }
