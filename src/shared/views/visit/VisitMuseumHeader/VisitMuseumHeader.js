70ecefa4fdfd3b596897a4ad565880031a3be9f2
Gerard Chin <gerardchin91@gmail.com>
2016-11-04 11:24:07 +1300

    Patch visit page flexing issue

--- src/shared/views/visit/VisitMuseumHeader/VisitMuseumHeader.js
+++ src/shared/views/visit/VisitMuseumHeader/VisitMuseumHeader.js
@@ -62,15 +62,17 @@ const VisitMuseumHeader = (props) => {
       <div className={styles.overlay}>
         <h1 className={styles.museumName}>{museumInfo.name}</h1>
         <p className={styles.museumTagline}>{museumInfo.tagline}</p>
-        <svg
-          fill={'#ffffff'}
-          viewBox={'0 0 24 24'}
-          xmlns={'http://www.w3.org/2000/svg'}
-          className={styles.downarrow}
-        >
-          <path d={'M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'} />
-          <path d={'M0 0h24v24H0z'} fill={'none'} />
-        </svg>
+        <div className={styles.arrowContainer}>
+          <svg
+            fill={'#ffffff'}
+            viewBox={'0 0 24 24'}
+            xmlns={'http://www.w3.org/2000/svg'}
+            className={styles.downarrow}
+          >
+            <path d={'M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z'} />
+            <path d={'M0 0h24v24H0z'} fill={'none'} />
+          </svg>
+        </div>
       </div>
     </header>
   )
