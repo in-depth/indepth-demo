0eceb838b2d2f6bdd3cbe34276b2b5f425b9bdbd
nickmask <nrmaskell@gmail.com>
2016-11-07 11:29:03 +1300

    Add collection items

--- src/shared/views/plans/planItinerary/PlanMap/PlanMap.js
+++ src/shared/views/plans/planItinerary/PlanMap/PlanMap.js
@@ -4,16 +4,8 @@ import styles from './PlanMap.css'
 
 const PlanMap = () => {
   return (
-    <div>
-      <div className={styles.header}>
-        <h2 className={styles.title}>Recommended Route</h2>
-      </div>
-      <p className={styles.subHeader}>
-        Thanks! We've put together a schedule for you.
-      </p>
-      <div className={styles.mapWrapper}>
-        <img className={styles.map} alt="map" src="/static/maps/Level2-plan.jpg" />
-      </div>
+    <div className={styles.mapWrapper}>
+      <img className={styles.map} alt="map" src="/static/maps/Level2-plan.jpg" />
     </div>
   )
 }
