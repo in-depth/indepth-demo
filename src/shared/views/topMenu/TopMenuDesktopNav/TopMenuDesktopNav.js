8abc46df342c8ecadbf2daa9cab2154a6713f71c
Gerard Chin <gerardchin91@gmail.com>
2016-11-07 11:53:25 +1300

    Add indexlink condition menu

--- src/shared/views/topMenu/TopMenuDesktopNav/TopMenuDesktopNav.js
+++ src/shared/views/topMenu/TopMenuDesktopNav/TopMenuDesktopNav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { Link, IndexLink } from 'react-router'
 import styles from './TopMenuDesktopNav.css'
 import Icon from '../../../components/Icon/Icon'
 
@@ -7,29 +7,33 @@ const TopMenuDesktopNav = ({ navs }) => {
   return (
     <nav className={styles.main}>
       <ul>
-        {navs.map((n, i) => (
-          <DesktopNavItem
-            key={i}
-            link={n.link}
-            title={n.title}
-            icon={n.icon}
-          />
-        ))}
+        {navs.map((n, i) => <DesktopNavItem key={i} {...n} />)}
       </ul>
     </nav>
   )
 }
 
-const DesktopNavItem = ({ link, title, icon }) => (
+const DesktopNavItem = ({ link, title, icon, main = false }) => (
   <li className={styles.navMain}>
-    <Link
-      to={link}
-      className={styles.navLink}
-      activeStyle={{ borderTop: '3px solid #fff' }}
-    >
-      <Icon icon={icon} />
-      <span>{title}</span>
-    </Link>
+    {main
+      ? <IndexLink
+        to={link}
+        className={styles.navLink}
+        activeStyle={{ borderTop: '3px solid #fff' }}
+      >
+        <Icon icon={icon} />
+        <span>{title}</span>
+      </IndexLink>
+      : <Link
+        to={link}
+        className={styles.navLink}
+        activeStyle={{ borderTop: '3px solid #fff' }}
+      >
+        <Icon icon={icon} />
+        <span>{title}</span>
+      </Link>
+    }
+
   </li>
 )
 
@@ -41,6 +45,7 @@ DesktopNavItem.propTypes = {
   icon: React.PropTypes.string.isRequired,
   title: React.PropTypes.string.isRequired,
   link: React.PropTypes.string.isRequired,
+  main: React.PropTypes.bool,
 }
 
 export default TopMenuDesktopNav
