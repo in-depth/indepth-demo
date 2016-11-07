c8f92b9e7a1dd44bdfa5cc0965a181f84177a296
nickmask <nrmaskell@gmail.com>
2016-11-07 13:58:26 +1300

    Add maps

--- src/shared/views/map/MapTabs/MapTabs.js
+++ src/shared/views/map/MapTabs/MapTabs.js
@@ -3,6 +3,8 @@ import { Tab, Tabs } from 'react-toolbox/lib/tabs'
 
 import { MapTab } from '../index'
 
+import styles from './MapTabs.css'
+
 class MapTabs extends React.Component {
   constructor() {
     super()
@@ -15,13 +17,18 @@ class MapTabs extends React.Component {
 
   render() {
     return (
-      <Tabs index={this.state.index} onChange={this.handleTabChange.bind(this)}>
-        {this.props.mapLevels.map((mapLevel) => (
-          <Tab label={mapLevel.title} key={mapLevel.title}>
-            <MapTab title={mapLevel.title} url={mapLevel.imageUrl} />
-          </Tab>
-        ))}
-      </Tabs>
+      <div className={styles.main}>
+        <div className={styles.header}>
+          <h1>Maps</h1>
+        </div>
+        <Tabs className={styles.tabs} index={this.state.index} onChange={this.handleTabChange.bind(this)}>
+          {this.props.mapLevels.map((mapLevel) => (
+            <Tab label={mapLevel.title} key={mapLevel.title}>
+              <MapTab title={mapLevel.title} url={mapLevel.imageUrl} />
+            </Tab>
+          ))}
+        </Tabs>
+    </div>
     )
   }
 }
