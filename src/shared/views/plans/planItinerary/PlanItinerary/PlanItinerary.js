35494858d058f45214576044ae0b4901edbd5b94
Gerard Chin <gerardchin91@gmail.com>
2016-11-07 13:55:27 +1300

    Fix scroll prob

--- src/shared/views/plans/planItinerary/PlanItinerary/PlanItinerary.js
+++ src/shared/views/plans/planItinerary/PlanItinerary/PlanItinerary.js
@@ -9,12 +9,6 @@ const items = ['APYPEBMF6X', 'CWVSA3MEVN', 'HNT3APBUNY']
 const PlanItinerary = (props) => {
   return (
     <div className={styles.main}>
-      <div className={styles.header}>
-        <h2 className={styles.title}>{'Recommended Route'}</h2>
-        <p className={styles.subHeader}>
-          {'Thanks! We\'ve put together a schedule for you.'}
-        </p>
-      </div>
       <PlanMap />
       <PlanItems items={items} intinerary={props.intinerary} />
     </div>
--- src/shared/views/plans/planItinerary/PlanMap/PlanMap.js
+++ src/shared/views/plans/planItinerary/PlanMap/PlanMap.js
@@ -4,8 +4,16 @@ import styles from './PlanMap.css'
 
 const PlanMap = () => {
   return (
-    <div className={styles.mapWrapper}>
-      <img className={styles.map} alt="map" src="/static/maps/Level2-plan.jpg" />
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <h2 className={styles.title}>{'Recommended Route'}</h2>
+        <p className={styles.subHeader}>
+          {'Thanks! We\'ve put together a schedule for you.'}
+        </p>
+      </div>
+      <div className={styles.mapWrapper}>
+        <img className={styles.map} alt="map" src="/static/maps/Level2-plan.jpg" />
+      </div>
     </div>
   )
 }
